ecce2b0e9aa5aa99a28512c39f957ac114b39959
capgravity <capacitorgravity@gmail.com>
2024-11-23 20:16:24 +0500

    Final Commit

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -14,6 +15,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname= path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,7 +24,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
+app.get("*", (req,res) =>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
